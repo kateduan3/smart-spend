658e573907095b456f2172e565231c16994f7233
kateduan <tianyi.duan@mail.utoronto.ca>
2024-09-21 19:24:38 -0400

    added scrollable homepage content

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -12,10 +12,40 @@ function Home() {
           <button className="register-button">Register Now</button>
         </Link>
         <Link to="/upload">
-          <button>Upload CSV</button>
-        </Link>
+              <button className="upload-button">Upload</button>
+            </Link>
       </header>
-      {/* Main content can go here */}
+
+      {/* Scrollable container */}
+      <div className="home-scroll-container">
+        {/* First home-top section */}
+        <div className="home-top">
+          <div className="main-content">
+            <h1 className="big-text">Welcome to SmartSpend</h1>
+            <p className="main-description">Take control of your finances by tracking your expenses and budget with ease.</p>
+            <Link to="/login">
+              <button className="register-button">Get Started</button>
+            </Link>
+          </div>
+          <div className="image-section">
+            <img src={process.env.PUBLIC_URL + '/static/illustration.png'} alt="Illustration" className="illustration-img" />
+          </div>
+        </div>
+
+        {/* Second home-top section (another set of content for scrolling) */}
+        <div className="home-top">
+          <div className="main-content">
+            <h1 className="big-text">Safe and Secure</h1>
+            <p className="main-description">Your Information is safe with us. We will not ask you to provide any sensitive data.</p>
+            <Link to="/login">
+              <button className="register-button">Get Started</button>
+            </Link>
+          </div>
+          <div className="image-section">
+            <img src={process.env.PUBLIC_URL + '/static/illustration2.png'} alt="Second Illustration" className="illustration-img" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
